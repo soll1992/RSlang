e28b5f492391f43464f2b7271002c7f3113c32a2
Anton <dr.dud34@gmail.com>
2022-02-06 16:53:29 +0300

    feat: add combo counter and score counter

--- src/components/sprint-game/sprint-game.tsx
+++ src/components/sprint-game/sprint-game.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addMoney, getMoney, addName, getName } from '../../redux/actions/actions';
 import axios, { AxiosResponse } from 'axios';
-import shuffle from 'lodash/shuffle'
-import random from 'lodash/random'
-import Button from '../button/button'
+import shuffle from 'lodash/shuffle';
+import random from 'lodash/random';
+import Button from '../button/button';
+import './sprint-game.scss'
 
 interface RootState {
   gameWordPage: {
@@ -41,68 +42,118 @@ export default function Sprint() {
   const [word, setWord] = useState('');
   const [translation, setTranlation] = useState('');
   const [answer, setAnswer] = useState(false);
+  const [currentWordnumber, setCurrentWordnumber] = useState(0);
+  const [trueAnswersNumber, setTrueAnswersNumber] = useState(0);
+  const [comboCounter, setComboCounter] = useState(0);
+  const [score, setScore] = useState(0);
+  const [scorMultiplier, setScorMultiplier] = useState(1);
+  const circle1 : React.MutableRefObject<HTMLDivElement | null> = useRef(null);
+  const circle2 : React.MutableRefObject<HTMLDivElement | null> = useRef(null);
+  const circle3 : React.MutableRefObject<HTMLDivElement | null> = useRef(null);
+  const comboRow = [circle1, circle2, circle3]
 
   useEffect(() => {
     getWords();
   }, []);
 
-  function wordCounter() {
-    let count = 0;
-    if (count >= 20) {
-      count = 0
-    }
-    return function (arr: WordData[]) {
-      let isTrue = Boolean(random(0,1))
-      if (isTrue) {
-        setAnswer(true)
-        setWord(arr[count].word)
-        setTranlation(arr[count].wordTranslate)
-        count++
-      } else {
-        setAnswer(false)
-        const randomPosition = random(0, 19)
-        setWord(arr[count].word)
-        setTranlation(arr[randomPosition].wordTranslate)
-        count++
-      }
+  function generateQuestion(arr: WordData[]) {
+    let isTrue = Boolean(random(0, 1));
+    if (isTrue) {
+      setAnswer(true);
+      setWord(arr[currentWordnumber].word);
+      setTranlation(arr[currentWordnumber].wordTranslate);
+      currentWordnumber === 19 ? setCurrentWordnumber(0) : setCurrentWordnumber(currentWordnumber + 1);
+    } else {
+      setAnswer(false);
+      const randomPosition = random(0, 19);
+      setWord(arr[currentWordnumber].word);
+      setTranlation(arr[randomPosition].wordTranslate);
+      currentWordnumber === 19 ? setCurrentWordnumber(0) : setCurrentWordnumber(currentWordnumber + 1);
     }
   }
 
-  const generateQuestion = wordCounter()
-
   function generateWords(res: AxiosResponse): WordData[] {
-    const dataArr = res.data
-    const shuffledData = shuffle(dataArr)
-    generateQuestion(shuffledData)
-    return shuffledData     
+    const dataArr = res.data;
+    const shuffledData = shuffle(dataArr);
+    generateQuestion(shuffledData);
+    return shuffledData;
   }
 
   function getWords() {
     axios
       .get<WordData[]>(`${baseUrl}/words?page=${page}&group=${difficulty}`)
       .then((res) => generateWords(res))
-      .then(result => setWordsData(result))
+      .then((result) => setWordsData(result))
       .catch((err) => console.log('error'));
   }
 
+  function removeCombo(combo: React.MutableRefObject<HTMLDivElement | null>[]) {
+    combo.forEach(item => {
+      item.current !== null && (item.current).classList.contains('green')
+      && (item.current).classList.remove('green')
+  })}
+
+  function scoreCounter () {
+    if (comboCounter === 3) {
+      setScorMultiplier(scorMultiplier * 2)
+      setScore(score + 10 * scorMultiplier * 2)
+    } else {
+      setScore(score + 10 * scorMultiplier)
+    }
+  }
+
+  function comboChecker() {
+    console.log(comboCounter)
+    if (comboCounter === 3) {
+      removeCombo(comboRow)
+      setComboCounter(1)
+      comboRow[0].current !== null && (comboRow[0].current as HTMLDivElement).classList.add('green');
+    } else {
+      comboRow[comboCounter].current !== null && (comboRow[comboCounter].current as HTMLDivElement).classList.add('green');
+      setComboCounter(comboCounter + 1)
+    }
+  }
+
+  function checkUserAnswer(userAnswer: boolean) {
+    switch(userAnswer === answer) {
+      case true: 
+        setTrueAnswersNumber(trueAnswersNumber + 1)
+        comboChecker()
+        scoreCounter ()
+        break;
+      case false: 
+        removeCombo(comboRow)
+        setComboCounter(0)
+        break;
+    }
+  }
+
   function trueButtonHandler() {
-    let userAnswer = true
-    userAnswer === answer ? alert('Верно') : alert('Неверно')
-    getWords()
+    let userAnswer = true;
+    checkUserAnswer(userAnswer);
+    generateQuestion(wordsData);
   }
 
   function falseButtonHandler() {
-    let userAnswer = false
-    userAnswer === answer ? alert('Верно') : alert('Неверно')
-    getWords()
+    let userAnswer = false;
+    checkUserAnswer(userAnswer);
+    generateQuestion(wordsData);
   }
 
   return (
-    <div>
+    <section>
+      <div>{`Правильных ответов: ${trueAnswersNumber} из 20`}</div>
+      <div>{`Счет: ${score}`}</div>
+      <div>{`Комбо множитель x${scorMultiplier}`}</div>
+      <div className="combo-row">
+        <div ref={circle1} className='circle'></div>
+        <div ref={circle2} className='circle'></div>
+        <div ref={circle3} className='circle'></div>
+      </div>
       <div>{word}</div>
       <div>{translation}</div>
-      <Button onClick={trueButtonHandler} class='button' textContent='Верно'/>
-      <Button onClick={falseButtonHandler} class='button' textContent='Неверно'/>      
-    </div>
+      <Button onClick={trueButtonHandler} class="button" textContent="Верно" />
+      <Button onClick={falseButtonHandler} class="button" textContent="Неверно" />
+    </section>
   );
 }
